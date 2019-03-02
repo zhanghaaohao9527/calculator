f39ebd602af4143f7c1488f1c07c350c529d4e50
zhanghaaohao9527 <2506681329@qq.com>
2019-03-02 15:58:16 +0800

    twoIntAdd sample

---
+++ src/add_two_ints_server.cpp
@@ -0,0 +1,24 @@
+#include "ros/ros.h"
+#include "calculator/AddTwoInts.h"   //  beginner_tutorials/AddTwoInts.h是由编译系统自动根据我们先前创建的srv文件生成的对应该srv文件的头文件。 
+
+/* 这个函数提供两个int值求和的服务，int值从request里面获取，而返回数据装入response内，这些数据类型都定义在srv文件内部，函数返回一个boolean值。 */
+bool add(calculator::AddTwoInts::Request  &req,
+         calculator::AddTwoInts::Response &res)
+{
+  res.sum = req.a + req.b;
+  ROS_INFO("request: x=%ld, y=%ld", (long int)req.a, (long int)req.b);
+  ROS_INFO("sending back response: [%ld]", (long int)res.sum);
+  return true;
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "add_two_ints_server");
+  ros::NodeHandle n;
+
+  ros::ServiceServer service = n.advertiseService("add_two_ints", add);
+  ROS_INFO("Ready to add two ints.");
+  ros::spin();
+
+  return 0;
+}
