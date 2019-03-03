54a4a2cade02fc7ce72b472551ad5e6302684975
zhanghaaohao9527 <2506681329@qq.com>
2019-03-03 18:46:14 +0800

    test add2Int fix

--- src/test/utest.cpp
+++ src/test/utest.cpp
@@ -4,27 +4,27 @@
 using namespace std;
 TEST(addTwoIntsServer, normal){
 	cout << "testCase1 "; // 输出 testCase1
-	EXPECT_EQ(1, add(1,5));
-    	EXPECT_EQ(1, add(-1,-1));
-	EXPECT_EQ(1, add(-1,1));
+	EXPECT_EQ(6, add(1,5));
+    	EXPECT_EQ(-2, add(-1,-1));
+	EXPECT_EQ(0, add(-1,1));
    	 EXPECT_EQ(1, add(9,-8));
 }
 
 TEST(addTwoIntsServer, zero){
 	 cout << "testCase2 "; // 输出 testCase2
-	EXPECT_EQ(2, add(0,0));
-	EXPECT_EQ(2, add(0,1));
-	EXPECT_EQ(2, add(-1,0));
+	EXPECT_EQ(0, add(0,0));
+	EXPECT_EQ(1, add(0,1));
+	EXPECT_EQ(-1, add(-1,0));
 }
 
 TEST(addTwoIntsServer, wrong){
 	 cout << "testCase3 "; // 输出 testCase3
-	EXPECT_EQ(3, add(a,b));
-        EXPECT_EQ(3, add(0.1,1));
-        EXPECT_EQ(3, add(99,0.2));
-	EXPECT_EQ(1, add(a,1));
-        EXPECT_EQ(1, add(1,z));
-        EXPECT_EQ(1, add(+,-));
+	EXPECT_EQ(NULL, add(a,b));
+        EXPECT_EQ(NULL, add(0.1,1));
+        EXPECT_EQ(99, add(99,0.2));
+	EXPECT_EQ(NULL, add(a,1));
+        EXPECT_EQ(NULL, add(1,z));
+        EXPECT_EQ(NULL, add(+,-));
 }
 
 
